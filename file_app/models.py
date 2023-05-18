bb717eb856f6e0a425cd2fa00831ea7a635f6880
Adil Alami <adilalami707@gmail.com>
2023-05-18 12:58:38 +0100

    the api almost ready

---
+++ file_app/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+class File(models.Model):
+  FRUIT = 'fruit'
+  LEAF = 'leaf'
+  TYPE_CHOICES = [(FRUIT, 'Fruit'), (LEAF, 'Leaf')]
+  file = models.FileField(blank=False, null=False)
+  remark = models.CharField(max_length=20)
+  type = models.CharField(max_length=5, choices=TYPE_CHOICES, default='fruit', null=False)
+  name = models.CharField(max_length=20, default='none', null=False)
+  timestamp = models.DateTimeField(auto_now_add=True)
---
+++ file_app/views.py
@@ -0,0 +1,89 @@
+from rest_framework.views import APIView
+from rest_framework.parsers import MultiPartParser, FormParser
+from rest_framework.response import Response
+from rest_framework import status
+from .serializers import FileSerializer
+
+import tensorflow as tf
+from tensorflow import keras
+import numpy as np
+import json
+
+
+class FileView(APIView):
+    parser_classes = (MultiPartParser, FormParser)
+
+    def post(self, request, *args, **kwargs):
+        file_serializer = FileSerializer(data=request.data)
+        if file_serializer.is_valid():
+            file_serializer.save()
+
+            dic_res = {}
+            path = file_serializer.data['file']
+            path_mqad = path[1:]
+            
+            # load the fruits model
+            def load_model_fruit():
+                return keras.models.load_model('assets/plantai_model_adil.h5')
+            # load the leafs model
+            def load_model_leaf(model_name):
+                return keras.models.load_model(f'Models/{model_name}___Leaf.h5')
+            # load the fruit classes
+            def load_classes_fruit():
+                with open('assets/classes.txt', 'r') as f:
+                    class_names = f.read().splitlines()
+                return class_names
+            # load the leaf classes
+            def load_classes_leaf(classes_name):
+                with open(f'Classes/{classes_name}___Classes.txt', 'r') as f:
+                    class_names = f.read().splitlines()
+                return class_names
+            # load the image and preprocess it
+            def load_prep(img_path):
+                img = tf.io.read_file(img_path)
+                img = tf.image.decode_image(img)
+                img = tf.image.resize(img, size=(224, 224))
+                return img
+            
+            # Make predictions  
+            def make_prediction(model, image, classes):
+                pred = model.predict(tf.expand_dims(image, axis=0))
+                pred_idx = np.argmax(pred)
+                predicted_value = classes[pred_idx]
+                return predicted_value
+            
+            
+            if file_serializer.data['type'] == 'fruit': # Check if the user wants fruits
+                image = load_prep(path_mqad)
+                model = load_model_fruit()
+                classes = load_classes_fruit()
+                prediction = make_prediction(model=model, classes=classes, image=image)
+                
+                # result
+                result = prediction.split(sep="___")
+                dic_res["Name"] = result[0].title()
+                dic_res["Condition"] = result[1].title()
+                dic_res["Type"] = result[2].title()
+                
+            elif file_serializer.data['type'] == 'leaf': # Check if the user wants leaves
+                image = load_prep(path_mqad)
+                model = load_model_leaf(model_name=file_serializer.data['name'].title())
+                classes = load_classes_leaf(classes_name=file_serializer.data['name'].title())
+                prediction = make_prediction(model=model, classes=classes, image=image)
+                
+                # result
+                result = prediction.split(sep="___")
+                dic_res["Name"] = result[0].title()
+                dic_res["Condition"] = result[1].title()
+                dic_res["Type"] = file_serializer.data['type'].title()
+            else:
+                return Response("Type is not valid", status=status.HTTP_400_BAD_REQUEST)
+            
+        
+            img_url = file_serializer.data['file']
+            
+            dic_res["image_url"] = img_url
+            
+            return Response(dic_res, status=status.HTTP_201_CREATED)
+        else:
+            return Response(file_serializer.errors, status=status.HTTP_400_BAD_REQUEST)

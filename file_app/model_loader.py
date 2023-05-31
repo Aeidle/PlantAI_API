0b169dccb7c8799475104db34be3ee90725c41dc
Aeidle <adilalami707@gmail.com>
2023-05-31 21:28:46 +0100

    minor chnages to the code

--- file_app/model_loader.py
+++ file_app/model_loader.py
@@ -5,17 +5,33 @@ import tensorflow as tf
 import numpy as np
 
 def load_model_fruit():
-    return keras.models.load_model('assets/PlantAI_Model_Fruit_EfficientNetB0.h5')
+    try:
+        return keras.models.load_model('Models/PlantAI_Model_Fruit_EfficientNetB0.h5')
+    except Exception as e:
+        print("Error loading fruit model:", str(e))
+        return None
 
 def load_model_leaf(model_name):
-    return keras.models.load_model(f'Models/{model_name}___Leaf.h5')
+    try:
+        return keras.models.load_model(f'Models/{model_name}___Leaf.h5')
+    except Exception as e:
+        print("Error loading leaf model:", str(e))
+        return None
 
 def load_classes_fruit():
-    with open('assets/classes.txt', 'r') as f:
-        class_names = f.read().splitlines()
-    return class_names
+    try:
+        with open('Classes/Classes.txt', 'r') as f:
+            class_names = f.read().splitlines()
+        return class_names
+    except Exception as e:
+        print("Error loading fruit classes:", str(e))
+        return None
 
 def load_classes_leaf(classes_name):
-    with open(f'Classes/{classes_name}___Classes.txt', 'r') as f:
-        class_names = f.read().splitlines()
-    return class_names
+    try:
+        with open(f'Classes/{classes_name}___Classes.txt', 'r') as f:
+            class_names = f.read().splitlines()
+        return class_names
+    except Exception as e:
+        print("Error loading leaf classes:", str(e))
+        return None
--- file_app/model_utils.py
+++ file_app/model_utils.py
@@ -2,38 +2,6 @@ from tensorflow import keras
 import tensorflow as tf
 import numpy as np
 
-def load_model_fruit():
-    try:
-        print(f"loading model PlantAI_Model_Fruit_EfficientNetB0.h5")
-        return keras.models.load_model('assets/PlantAI_Model_Fruit_EfficientNetB0.h5')
-    except Exception as e:
-        raise Exception("Failed to load fruit model: " + str(e))
-
-def load_model_leaf(model_name):
-    try:
-        print(f"loading model {model_name}___Leaf.h5")
-        return keras.models.load_model(f'Models/{model_name}___Leaf.h5')
-    except Exception as e:
-        raise Exception("Failed to load leaf model: " + str(e))
-
-def load_classes_fruit():
-    try:
-        print(f"loading fruit classes")
-        with open('assets/classes.txt', 'r') as f:
-            class_names = f.read().splitlines()
-        return class_names
-    except Exception as e:
-        raise Exception("Failed to load fruit classes: " + str(e))
-
-def load_classes_leaf(classes_name):
-    try:
-        print(f"loading classe {classes_name}___Leaf.h5")
-        with open(f'Classes/{classes_name}___Classes.txt', 'r') as f:
-            class_names = f.read().splitlines()
-        return class_names
-    except Exception as e:
-        raise Exception("Failed to load leaf classes: " + str(e))
-
 def load_prep(img_path):
     try:
         print(f"loading image")

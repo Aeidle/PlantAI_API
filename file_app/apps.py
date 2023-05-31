6cc571b74baa9d13d79546bdf93b473ac1dca5c6
Aeidle <adilalami707@gmail.com>
2023-05-31 19:27:16 +0100

    minor changes

--- file_app/apps.py
+++ file_app/apps.py
@@ -1,31 +1,3 @@
 from django.apps import AppConfig
-
 class FileAppConfig(AppConfig):
     name = 'file_app'
-
-    # def ready(self):
-    #     from .model_utils import load_model_fruit, load_classes_fruit, load_model_leaf, load_classes_leaf
-    #     load_model_fruit()
-    #     load_classes_fruit()
-    #     load_model_leaf('Apple')
-    #     load_model_leaf('Bell_Pepper')
-    #     load_model_leaf('Broad_Bean')
-    #     load_model_leaf('Corn_Maize')
-    #     load_model_leaf('Eggplant')
-    #     load_model_leaf('Grape')
-    #     load_model_leaf('Mango')
-    #     load_model_leaf('Orange')
-    #     load_model_leaf('Peach')
-    #     load_model_leaf('Potato')
-    #     load_model_leaf('Tomato')
-    #     load_classes_leaf('Apple')
-    #     load_classes_leaf('Bell_Pepper')
-    #     load_classes_leaf('Broad_Bean')
-    #     load_classes_leaf('Corn_Maize')
-    #     load_classes_leaf('Eggplant')
-    #     load_classes_leaf('Grape')
-    #     load_classes_leaf('Mango')
-    #     load_classes_leaf('Orange')
-    #     load_classes_leaf('Peach')
-    #     load_classes_leaf('Potato')
-    #     load_classes_leaf('Tomato')
